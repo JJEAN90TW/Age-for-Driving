dd9949d50bc139902d626ca0cd12a6981840635a
JJEAN <jennyjean423@gmail.com>
2019-02-26 14:52:54 +0800

    add Canada

--- age_for_driving.py
+++ age_for_driving.py
@@ -1,8 +1,13 @@
 country = input('請問你是哪國人：')
 age = input('請輸入年齡：')
 age = int(age)
-if country == '台灣' or '臺灣' or 'taiwan':
+if country == '台灣':
     if age >= 18:
         print('你可以在台灣考駕照/開車')
     else:
         print('你還不能在台灣考駕照/開車')
+elif country == '加拿大':
+    if age >= 16:
+        print('你可以在加拿大考駕照/開車')
+    else:
+        print('你還不能在加拿大考駕照/開車')
